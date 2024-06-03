d7ca3cb26d2d231b58312342e79ea690d3fdaf39
sy-sinai <sammy.porras@udla.edu.ec>
2024-06-03 08:58:22 -0500

    Creacion de Picker

--- Recarga/Models/Recarga.xaml.cs
+++ Recarga/Models/Recarga.xaml.cs
@@ -4,10 +4,21 @@ namespace Recarga.Models;
 
 public partial class Recarga : ContentPage
 {
+    int uno = 1;
 	public Recarga()
 	{
 		InitializeComponent();
-	}
+        var monkeyList = new List<string>();
+        monkeyList.Add("Tuenti");
+        monkeyList.Add("Movistar");
+        monkeyList.Add("Claro");
+        monkeyList.Add("Maxiplus");
+        
+        Picker picker = new Picker { Title = "Select a monkey" };
+        picker.ItemsSource = monkeyList;
+    }
+
+    
     async void SaveButton_Clicked(object sender, EventArgs args)
     {
   
